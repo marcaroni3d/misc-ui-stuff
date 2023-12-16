b1a91405ba422872c7a047ef4222ba88fffac58d
marcaroni3d <marc.eddington@gmail.com>
2023-12-16 16:43:03 -0500

    version 1.4: added sidebar

--- modules/UI.js
+++ modules/UI.js
@@ -1,7 +1,9 @@
 const cssNavbarButton = document.querySelector("[data-css-navbar-button]")
 const jsNavbarButton = document.querySelector("[data-js-navbar-button]")
+const sideMenuButton = document.querySelector("[data-side-menu-button]")
 const cssNavbar = document.querySelector(".css-navbar")
 const jsNavbar = document.querySelector(".js-navbar")
+const menubar = document.querySelector(".left-menubar")
 
 export default function initUI() {
     cssNavbarButton.addEventListener('click', () => {
@@ -12,10 +14,20 @@ export default function initUI() {
         toggleDisplay(jsNavbar)
         jsNavbarButton.classList.toggle('active')
     })
+    sideMenuButton.addEventListener('click', () => {
+        toggleDisplay(menubar)
+        sideMenuButton.classList.toggle('active')
+    })
 }
 
 function toggleDisplay(target) {
-    target.style.display = 
+    if (target.classList.contains("left-menubar")) {
+        target.style.display = 
+        (target.style.display === 'none' || target.style.display === '')
+        ? 'flex' : 'none'
+    } else {
+        target.style.display = 
         (target.style.display === 'none' || target.style.display === '')
         ? 'block' : 'none'
+    }
 }
